a4709bf3356fe0c5b461200751cb27ad273bd288
chienpv <phamvanbk11@gmail.com>
2023-11-02 10:44:09 +0700

    init project

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let foo = "test";
+let bar = 1;
+let baz = ["This", "is", "a", "Test"];
+let a = 1;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let foo: string = "test";
+let bar: number = 1;
+let baz: string[] = ["This", "is", "a", "Test"];
+let a: number = 1;
